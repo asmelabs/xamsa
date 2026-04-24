32334419900460879332965149e4d9d4ff63d772
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-04-24 15:34:14 +0400

    feat: add icons

---
+++ packages/utils/src/badges.ts
@@ -0,0 +1,8 @@
+// Badges are given to a player/host per topic, game, or as a lifetime achievement
+
+export type Badge = {
+	name: string;
+	description: string;
+	icon: string;
+	color: string;
+};
